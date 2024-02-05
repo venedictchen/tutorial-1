a0f2bc39ecfe6aedd4956c2c715212b47ec74ba8
venedict chen <venedictchen@gmail.com>
2024-02-05 16:33:55 +0700

    feat: edit product and change delete to @DeleteMapping

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -7,4 +7,6 @@ public interface ProductService {
     public Product create(Product product);
     public List<Product> findAll();
     public void delete(String productId);
+    public Product get(String productId);
+    public void update(Product productUpdate);
 }
