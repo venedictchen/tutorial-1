6cf2a98a7c16e04c9aef63af9f14f7977c110cca
venedict chen <venedictchen@gmail.com>
2024-03-06 17:44:51 +0700

    [REFACTOR] Add PaymenntStatus and PaymentMethod enum

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 
 import java.util.Map;
@@ -12,30 +13,31 @@ public class Payment {
     String method;
     Map<String, String> paymentData;
     Order order;
+    String status;
 
     public Payment(String id,  Order order, String method, Map<String, String>paymentData){
         this(order, method, paymentData);
         this.id = id;
+
     }
 
     public Payment(Order order, String method, Map<String, String>paymentData){
         this.id = UUID.randomUUID().toString();
         this.method = method;
         this.order = order;
-        this.status=PaymentStatus.WAITING_PAYMENT.getValue();
-        this.setPaymentData(paymentData);
-    }
-
-    public void setStatus(String status){
-        if(PaymentStatus.contains(status)){
-            this.status=status;
-        }else{
+        try{
+            setPaymentData(paymentData);
+            setStatus(PaymentStatus.SUCCESS.getValue());
+        }catch (IllegalArgumentException e){
+            setStatus(PaymentStatus.REJECTED.getValue());
             throw new IllegalArgumentException();
         }
     }
-
+    private void setStatus(String status){
+        this.status=status;
+    }
     private void setPaymentData(Map<String, String>paymentData){
-        if (method.equals("VOUCHER")){
+        if (method.equals(PaymentMethod.VOUCHER.getValue())){
             int numOfNumerics = 0;
             for (int i=0; i<paymentData.get("voucherCode").length(); i++){
                 if (Character.isDigit(paymentData.get("voucherCode").charAt(i))){
