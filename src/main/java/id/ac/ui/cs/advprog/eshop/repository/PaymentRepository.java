4c1dffe99b0abbed248f364ba48c41211a721632
venedict chen <venedictchen@gmail.com>
2024-03-06 18:09:07 +0700

    [GREEN] implement PaymentRepository

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaymentRepository {
+    private List<Payment> paymentData = new ArrayList();
+
+    public Payment save(Payment payment){
+        for (Payment paymentData: paymentData){
+            if (paymentData.getId().equals(payment.getId())){
+                throw new IllegalStateException();
+            }
+        }
+        paymentData.add(payment);
+        return payment;
+    }
+    public Payment findById(String id){
+        for (Payment payment : paymentData){
+            if (payment.getId().equals(id)){
+                return payment;
+            }
+        }
+        return null;
+    }
+
+    public List<Payment> getAllPayments(){
+        return paymentData;
+    }
+}
