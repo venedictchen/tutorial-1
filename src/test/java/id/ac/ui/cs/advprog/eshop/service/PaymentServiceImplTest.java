7d71350e63889afff26f1264aee07c909cac73f2
venedict chen <venedictchen@gmail.com>
2024-03-06 18:22:44 +0700

    [RED] Add PaymentServiceTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImplTest.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
-import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
@@ -22,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
-public class PaymentServiceTest {
+public class PaymentServiceImplTest {
     @InjectMocks
     PaymentService paymentService;
     @Mock
